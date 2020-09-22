43044fe5d4c515db051b3460fe74b8857c87ba44
10shu <shubham.singh1_cs18@gla.ac.in>
2020-09-23 01:33:08 +0530

    created class Player

--- musicplayer.py
+++ musicplayer.py
@@ -1,11 +1,17 @@
 import os
 import pickle
 import tkinter as tk
-from pygame import  mixer
+from pygame import mixer
 
-
+class Player(tk.Frame):      #player class
+    def __init__(self,master):
+        super().__init__(master)
+        self.master=master
+        self.pack()
 
 root=tk.Tk()
-root.geometery('600x400')
+root.geometry('600x400')
 root.wm_title('Music player')
-root.mainloop()
+
+app=Player(master=root)
+app.mainloop()
