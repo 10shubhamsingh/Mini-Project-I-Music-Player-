30cd7fdbeaefc6b4997f07f0f840cf6ffe56e4d8
10shu <shubham.singh1_cs18@gla.ac.in>
2020-11-05 01:08:52 +0530

    added image to first frame

--- musicplayer.py
+++ musicplayer.py
@@ -2,6 +2,8 @@ import os
 import pickle
 import tkinter as tk
 from pygame import mixer
+from tkinter import filedialog
+from tkinter import PhotoImage
 
 class Player(tk.Frame):      #player class
     def __init__(self,master):
@@ -30,13 +32,24 @@ class Player(tk.Frame):      #player class
         self.tracklist.grid(row=0, column=1, rowspan=3,pady=5)
 
         self.controls = tk.LabelFrame(self,
-                                   font=("Times New Roman", 14, "bold"),
+                                   font=("Times New Roman", 15, "bold"),
                                    bg="white", fg="white", bd=2, relief=tk.GROOVE)
         self.controls.configure(width=410, height=80)
         self.controls.grid(row=2, column=0,pady=5,padx=10)
 
     def track_widgets(self):
-        pass
+
+        self.canvas= tk.Label(self.track, image=img)
+        self.canvas.configure(width=400, height=240)
+        self.canvas.grid(row=0, column=0)
+
+        self.canvas= tk.Label(self.track, font=("Times New Roman", 15, "bold"),
+                              bg='white',fg='dark blue')
+        self.canvas['text']='Music Player'
+        self.canvas.configure(width=30, height=1)
+        self.canvas.grid(row=1, column=0)
+
+
 
     def control_widgets(self):
         pass
@@ -48,6 +61,8 @@ class Player(tk.Frame):      #player class
 root=tk.Tk()
 root.geometry('600x400')
 root.wm_title('Music player')
+
+img= PhotoImage(file='images/music.gif')
 
 app=Player(master=root)
 app.mainloop()
